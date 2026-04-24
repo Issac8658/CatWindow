be1d8fee65027e96f0acbd2471b917b54cdf09bb
Issac332 <119879937+Issac8658@users.noreply.github.com>
2026-04-24 16:47:54 +0300

    wave visualizator script cleaning

--- Godot/scripts/cat/visualizators/WaveVisualizator.cs
+++ Godot/scripts/cat/visualizators/WaveVisualizator.cs
@@ -1,10 +1,7 @@
 using Godot;
-using System;
 
 public partial class WaveVisualizator : Node2D
 {
-	[Export]
-	public float ShakePower = 20f;
 	[Export]
 	public float WaveScale = 8;
 	[Export]
@@ -21,17 +18,11 @@ public partial class WaveVisualizator : Node2D
 	// La# - 412 (Semitone 10)
 	// Si  - 389 (Semitone 11)
 	public int MinFramesCount = 1024;
-	[Export]
-	public FFmpeg.FFmpegPlayer Player;
-
-	private Vector2I _lastShake = new();
 
 	private AudioEffectCapture Capture = AudioServer.GetBusEffect(2, 0) as AudioEffectCapture;
-	private AudioEffectSpectrumAnalyzerInstance Spectrum = AudioServer.GetBusEffectInstance(2, 1) as AudioEffectSpectrumAnalyzerInstance;
 	private SubViewport _viewport;
 
 	private Godot.Collections.Array<Vector2> Buffer = [];
-	private Godot.Collections.Array<Vector2> CompressedBuffer = [];
 
 	public override void _Ready()
 	{
@@ -41,22 +32,10 @@ public partial class WaveVisualizator : Node2D
 	{
 		if (IsVisibleInTree())
 			QueueRedraw();
-		// window shaking
-		//if (false)
-		//{
-		//	float spec = Spectrum.GetMagnitudeForFrequencyRange(60, 1000).X;
-		//	Vector2I shake = (Vector2I)(new Vector2((float)GD.RandRange(-ShakePower, ShakePower), (float)GD.RandRange(-ShakePower, ShakePower)) * Mathf.Pow(spec, 2f));
-		//	GetWindow().Position += shake - _lastShake;
-		//	_lastShake = shake; // cat window, come back now
-		//}
 	}
 
 	public override void _Draw()
 	{
-
-		float Volume = 1;
-		float PoweredVolume = Mathf.Pow(Volume - 0.0025f, 0.125f); // for wave transparency
-
 		Vector2I viewportSize = _viewport.Size;
 
 		if (Capture.GetFramesAvailable() / MinFramesCount >= 1)
@@ -70,20 +49,16 @@ public partial class WaveVisualizator : Node2D
 
 		for (int i = 0; i < Buffer.Count; i += 1)
 		{
-			// left channel
-			float Sample1 = Buffer[Mathf.Clamp(i, 0, Buffer.Count - 1)].X;
-			float Y1 = viewportSize.Y / 2 + -Sample1 * viewportSize.Y / 2;
-
-			// right channel
-			float Sample2 = Buffer[Mathf.Clamp(i + 1, 0, Buffer.Count - 1)].Y;
-			float Y2 = viewportSize.Y / 2 + -Sample2 * viewportSize.Y / 2;
-
-			// Making lines red if amplitude >1
-			Color col = new(1, 1, 1, PoweredVolume);
-			if (MathF.Abs(Sample1) > 1f || MathF.Abs(Sample2) > 1f)
-				col = new Color(1, 0, 0, PoweredVolume);
-
-			DrawLine(new Vector2(i / WaveScale, Y1), new Vector2(i / WaveScale, Y2), col);
+			Vector2 Value1 = Buffer[Mathf.Clamp(i, 0, Buffer.Count - 1)];
+			Vector2 Value2 = Buffer[Mathf.Clamp(i + 1, 0, Buffer.Count - 1)];
+			if (Mathf.Abs(Value1.X) > 0.0005 || Mathf.Abs(Value2.Y) > 0.0005)
+			{
+				// left channel
+				float Y1 = viewportSize.Y / 2 + -Value1.X * viewportSize.Y / 2;
+				// right channel
+				float Y2 = viewportSize.Y / 2 + -Value2.Y * viewportSize.Y / 2;
+				DrawLine(new(i / WaveScale, Y1), new(i / WaveScale, Y2), new(1, 1, 1));
+			}
 		}
 	}
 }
