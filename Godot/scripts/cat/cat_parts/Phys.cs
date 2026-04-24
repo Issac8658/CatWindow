07816f777841e8c90f0ea382e13b6bb546fb374b
Issac332 <119879937+Issac8658@users.noreply.github.com>
2026-04-24 18:54:19 +0300

    custom paws physics
    Using custom paws physic instead of godot's physics

--- Godot/scripts/cat/cat_parts/Phys.cs
+++ Godot/scripts/cat/cat_parts/Phys.cs
@@ -1,15 +1,43 @@
 using Godot;
-using System;
 
-public partial class Phys : Node
+public partial class Phys : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export]
+	public float Mass = 1f;
+	[Export]
+	public float Length = 100;
+	[Export(PropertyHint.Range, "0,1,or_greater,or_less")]
+	public float GravityScale = 1;
+	[Export(PropertyHint.Range, "0,1,or_greater,or_less")]
+	public float WindowForceMultiplier = 1;
+	[Export(PropertyHint.Range, "0,1,or_greater,or_less")]
+	public float Damping = 0.5f;
+
+	private Vector2 OldPos;
+	private float AngularVelocity = 0;
+
 	public override void _Ready()
 	{
+		OldPos = GlobalPosition + GetWindow().Position;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
 	public override void _Process(double delta)
 	{
+		float dt = (float)delta;
+
+		Vector2 PhysPos = new(Length * Mathf.Sin(Rotation), Length * Mathf.Cos(Rotation));
+
+		Vector2 NewPos = GlobalPosition + GetWindow().Position;
+		Vector2 Force = -(NewPos - OldPos) * WindowForceMultiplier;
+		OldPos = NewPos;
+
+		float Torque = PhysPos.X * Force.Y - PhysPos.Y * Force.X;
+		
+		float AngularAcceleration = -(ProjectSettings.GetSetting("physics/2d/default_gravity", 0f).AsSingle() * GravityScale / Length)* Mathf.Sin(Rotation) + Torque / (Mass * Length * Length);
+
+		AngularVelocity += AngularAcceleration * dt;
+		AngularVelocity -= AngularVelocity * Damping * dt;
+		Rotation += AngularVelocity * dt;
 	}
 }
