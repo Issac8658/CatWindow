032f22f23e503775d381437c59ae867f0abe38ce
Issac332 <119879937+Issac8658@users.noreply.github.com>
2026-05-06 21:25:22 +0300

    audio recapture on audio device change

--- Godot/scripts/SystemAudioCapture.cs
+++ Godot/scripts/SystemAudioCapture.cs
@@ -1,20 +1,23 @@
 using Godot;
 using FFmpeg;
-using NAudio.Wave;
 using System;
 using NAudio.CoreAudioApi;
+using NAudio.Wave;
 
 public partial class SystemAudioCapture : Node
 {
-	private WasapiLoopbackCapture capture;
+	private static MMDeviceEnumerator enumerator = new();
+	private WasapiLoopbackCapture _capture;
 	private AudioStreamGeneratorPlayback _playback;
-
 	private AudioStreamPlayer _player;
+	private string _oldNAudioDevice;
+
 	[Export]
 	public int TargetBus = 0;
 
 	public override void _Ready()
 	{
+
 		_player = new() { Bus = AudioServer.GetBusName(TargetBus) };
 		AddChild(_player);
 
@@ -29,20 +32,54 @@ public partial class SystemAudioCapture : Node
 		_playback = _player.GetStreamPlayback() as AudioStreamGeneratorPlayback;
 
 		StartCapture();
+		CheckLoop();
 	}
 
-	public void StartCapture()
+	private void CheckLoop()
 	{
-		var device = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-		capture = new(device) 
+		string id = GetCurrentDevice().ID;
+		if (_oldNAudioDevice != id)
+		{
+			GD.Print("Device changed, recapturing...");
+			StartCapture();
+			_oldNAudioDevice = id;
+		}
+
+		GetTree().CreateTimer(1).Timeout += CheckLoop;
+	}
+
+	public void StartCapture(string DeviceOverride = null)
+	{
+		StopCapture();
+
+		MMDevice device;
+		
+		//GD.Print(DeviceOverride);
+		if (DeviceOverride == null || DeviceOverride == "Default")
+			device = GetCurrentDevice();
+		else
+			device = GetDeviceFromFriendlyName(DeviceOverride);
+
+		_capture = new(device)
 		{
 			ShareMode = AudioClientShareMode.Shared,
 			WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(48000, 2)
 		};
 
-		capture.DataAvailable += (s, e) => ProcessAudio(e.Buffer, e.BytesRecorded);
+		_capture.DataAvailable += (s, e) => ProcessAudio(e.Buffer, e.BytesRecorded);
 
-		capture.StartRecording();
+		_capture.StartRecording();
+		GD.Print("Capture started");
+	}
+	
+	public void StopCapture()
+	{
+		if (_capture != null)
+		{
+			_capture.StopRecording();
+			_capture = null;
+			GD.Print("Capture stopped");
+		}
 	}
 
 	private void ProcessAudio(byte[] buffer, int bytes)
@@ -52,5 +89,15 @@ public partial class SystemAudioCapture : Node
 		Buffer.BlockCopy(buffer, 0, samples, 0, bytes);
 
 		FFmpegPlayer.PushSamples(_playback, samples);
+	}
+
+	private static MMDevice GetCurrentDevice() => enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+	private static MMDevice GetDeviceFromFriendlyName(string FriendlyName)
+	{
+		var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+		foreach (var device in devices)
+			if (device.FriendlyName == FriendlyName) return device;
+		return GetCurrentDevice();
 	}
 }
--- Godot/scripts/audio_player/additional/output_selection.gd
+++ Godot/scripts/audio_player/additional/output_selection.gd
@@ -1,5 +1,7 @@
 extends OptionButton
 
+@export var audio_capture_node : Node
+
 var devices : PackedStringArray = AudioServer.get_output_device_list()
 var selected_device : String = "Default"
 
@@ -7,9 +9,12 @@ var selected_device : String = "Default"
 func _ready() -> void:
 	for device in devices:
 		add_item(device)
+
 	item_selected.connect(func (item):
 		selected_device = devices[item]
 		AudioServer.output_device = devices[item]
+		if (audio_capture_node):
+			audio_capture_node.StartCapture(selected_device)
 	)
 	mouse_entered.connect(func():
 		devices = AudioServer.get_output_device_list()
